ade66cee2cbe4439750f40deab6106b371f515ff
Eder Carvalho <91353220+Eder84Carlos@users.noreply.github.com>
2022-03-03 18:37:08 -0300

    Projeto Iniciado

---
+++ src/main/java/city_company/example/city_company/CityCompanyApplication.java
@@ -0,0 +1,13 @@
+package city_company.example.city_company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/city_company/example/city_company/CityCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package city_company.example.city_company;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
