ab11e502a70d39fd1d1e357e8bef3ef9a6a1c01b
Eder Carvalho <91353220+Eder84Carlos@users.noreply.github.com>
2022-03-05 00:06:14 -0300

    Projeto teste

--- src/main/java/city_company/example/city_company/resource/ClienteResource.java
+++ src/main/java/city_company/example/city_company/resource/ClienteResource.java
@@ -3,9 +3,12 @@ package city_company.example.city_company.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import city_company.example.city_company.entities.Cliente;
@@ -16,16 +19,17 @@ import city_company.example.city_company.reposotories.ClienteRepository;
 public class ClienteResource {
 	
 	@Autowired
-	private ClienteRepository repository;
+	private ClienteRepository clienteRepository;
 	
 	@GetMapping
-	public ResponseEntity<List<Cliente>> findAll(){
-		List<Cliente> list = repository.findAll();
-		return ResponseEntity.ok().body(list);
+	public List<Cliente> listar(){
+		return clienteRepository.findAll();
 	}
 	
-	public Cliente adicionar(Cliente cliente) {
-		return 
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
 	}
 
 }
